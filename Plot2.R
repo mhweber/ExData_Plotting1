f35e1d352c5bddfc7c9b714b778bd05c0e32c5f2
Marc Weber <mweber36@gmail.com>
2015-11-07 21:16:40 -0800

    Plot 2 commit

--- Plot2.R
+++ Plot2.R
@@ -7,11 +7,13 @@ str(hpc) # examine the data frame
 hpc = hpc[hpc$Date %in% c('1/2/2007','2/2/2007'),]
 hpc$Global_active_power = as.character(hpc$Global_active_power)
 hpc[hpc$Global_active_power=="?"] = NA
-hpc$Global_active_power = as.numeric(hpc$Global_active_power)
+
 head(hpc)
 tail(hpc)
-## Open png device; create 'Plot1.png' 
-png(file = 'Plot1.png',width = 480, height = 480)
-hist(hpc$Global_active_power,col="red", main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)")
+## Open png device; create 'Plot2.png' 
+png(file = 'Plot2.png',width = 480, height = 480)
+
+plot(hpc$Global_active_power ~ hpc$Time, xaxt='n',type = "l",
+     xlab="", ylab = "Global Active Power (kilowatts)")
+axis(1,at=c(0,700,1400), labels=c('Thur','Fri','Sat'))
 dev.off()
